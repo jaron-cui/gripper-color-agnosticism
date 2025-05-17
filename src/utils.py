00496cef90815cd70571e1fd98938a1b407e4ddc
jaroncui <jaron.cui@gmail.com>
2025-05-17 11:30:48 -0400

    add save_video()

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import json
 
+import cv2
 import decord
 import numpy as np
 
@@ -18,3 +19,10 @@ def load_gripper_values(reference_labels_path: str) -> np.ndarray:
     with open(reference_labels_path, 'r') as file:
         data = json.load(file)
     return np.array([entry['gripper'] for entry in data.values()])
+
+
+def save_video(frames: np.ndarray, output_path: str, fps: float = 30.00):
+    video = cv2.VideoWriter(output_path, -1, fps, (frames[0].shape[1], frames[0].shape[0]))
+    for frame in frames:
+        video.write(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    video.release()
